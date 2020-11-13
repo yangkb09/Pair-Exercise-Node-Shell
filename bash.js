93a437184cffccc14fb3f1bd4060b84a989a1f81
Jessica Lee <jessicalee546@gmail.com>
2020-11-13 15:33:11 -0500

    linking

--- bash.js
+++ bash.js
@@ -8,13 +8,15 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-    process.stdout.write('\nprompt > ');
-  } else {
-    process.stdout.write('You typed: ' + cmd);
+  // if (cmd === 'pwd') {
+  //   process.stdout.write(process.cwd());
+  //   process.stdout.write('\nprompt > ');
+  // } else {
+  process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
-  }
+  // }
 })
+
+const getPWD = require('./pwd')
 
 
---
+++ pwd.js
@@ -0,0 +1,7 @@
+module.exports = process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd') {
+      process.stdout.write(process.cwd());
+      process.stdout.write('\nprompt > ');
+    }
+  })
